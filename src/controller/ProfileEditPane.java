cc710da6abc072fa4119175717e24c430923fddc
ohhMyVenuss <minhhbn.24it@vku.udn.vn>
2025-05-05 15:29:42 +0700

    update setting pane with fxml

---
+++ src/controller/ProfileEditPane.java
@@ -0,0 +1,47 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+public class ProfileEditPane {
+    @FXML
+    private ImageView avatarImageView;
+    @FXML
+    private ImageView coverImageView;
+    @FXML
+    private Button chooseAvatarBtn;
+    @FXML
+    public void initialize() {
+//        Circle clip = new Circle(120, 120, 120);
+//        avatarImageView.setClip(clip);
+//        chooseAvatarBtn.setOnAction(e -> chooseAvatar());
+//        avatarImageView.setPreserveRatio(true);
+//        avatarImageView.setFitWidth(120);
+//        avatarImageView.setFitHeight(120);
+//        avatarImageView.layoutBoundsProperty().addListener((obs, oldVal, newVal) -> {
+//            double radius = Math.min(newVal.getWidth(), newVal.getHeight()) / 2;
+//            Circle clip = new Circle(radius, radius, radius);
+//            avatarImageView.setClip(clip);
+//        });
+        avatarImageView.setFitWidth(120);
+        avatarImageView.setFitHeight(120);
+        avatarImageView.setPreserveRatio(false); // Đảm bảo ảnh fill hết vùng
+
+        avatarImageView.layoutBoundsProperty().addListener((obs, oldVal, newVal) -> {
+            double radius = Math.min(newVal.getWidth(), newVal.getHeight()) / 2;
+            Circle clip = new Circle(radius, radius, radius);
+            avatarImageView.setClip(clip);
+        });
+        chooseAvatarBtn.setOnAction(e -> chooseAvatar());
+    }
+
+    public void chooseAvatar() {
+
+    }
+
+
+}
